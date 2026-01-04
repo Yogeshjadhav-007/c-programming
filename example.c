5ba217e934e3fa506619bdfced82ee368ff728a3
yj35966-ops <yj35966@gmail.com>
2026-01-04 21:45:25 +0530

    examples

---
+++ example.c
@@ -0,0 +1,16 @@
+#include<stdio.h>
+int main(){
+    // int a,b,sum;
+    // a=10;
+    // b=5;
+    // sum=a+b;
+    // printf("addition of two no is = %d",sum);
+    int a,b;
+    printf("enter first number \n");
+    scanf("%d",&a);
+    printf("enter second number \n");
+    scanf("%d",&b);
+    printf("addtion=%d",a + b);
+    return 0;
+}
+
---
+++ exaples_of.c
@@ -0,0 +1,29 @@
+#include<stdio.h>
+int main(){
+//	arithmetic opertaor
+
+//	int a,b;
+//	printf("enter a first number");
+//	scanf("%d",&a);
+//	printf("enter a second number");
+//	scanf("%d",&b);
+//	printf("addtion of two no is:%d",a+b);
+
+//assignment operator
+//	int age=25;
+//	age += 21;
+//	printf("age = %d",age);
+
+//	comparison operator
+//	int x=10;
+//	int y=22;
+//	printf("%d",x>y);
+//	logical operator   
+//	int yogesh=10;
+//	int jadhav=2;
+//	printf("yogesh=%d",yogesh>jadhav&&jadhav<yogesh);
+	int numbers[]={2,4,1,5,8,2,7,9,55};
+	printf("%zu",numbers[2]);
+	
+	return 0;
+}
